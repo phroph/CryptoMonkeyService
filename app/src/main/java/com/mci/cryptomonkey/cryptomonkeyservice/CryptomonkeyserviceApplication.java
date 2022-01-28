1312186ffb3adbffb8c23ff2a05e5a98d00c875e
Phillip Huff <phroph@users.noreply.github.com>
2022-01-28 17:16:20 -0500

    Adding Spring Boot template
    Adding in the baseline Spring Boot template generated by the Spring initializer ontop of the CDK template

---
+++ app/src/main/java/com/mci/cryptomonkey/cryptomonkeyservice/CryptomonkeyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mci.cryptomonkey.cryptomonkeyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptomonkeyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptomonkeyserviceApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/mci/cryptomonkey/cryptomonkeyservice/CryptomonkeyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mci.cryptomonkey.cryptomonkeyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptomonkeyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
